a165229e3e47a551a4f9a37a439cd5747e1e8d8f
jegomez <go.juangomez23@gmail.com>
2021-08-30 12:00:27 -0500

    se agrega dasys.js para manejo de fechas

--- bundle/amortizaciones/infrastructure/repository/nexShedulePaymentsRepository.js
+++ bundle/amortizaciones/infrastructure/repository/nexShedulePaymentsRepository.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const  fs = require('fs')
+const dayjs = require('dayjs')
 module.exports = async (Postgres) => {  
   const sql = fs.readFileSync(path.resolve(__dirname,'.','sql','nexShedulePayments.sql'),'utf8')
   const options = {
     type: Postgres.QueryTypes.SELECT,
     replacements:{
-      startDate: '2021-08-01',
-      finishDate: '2021-08-31'
+      startDate: dayjs().format('YYYY-MM-DD'),
+      finishDate: dayjs().add(7, 'day').format('YYYY-MM-DD')
     }
   }
   return  await Postgres.query(sql, options)
