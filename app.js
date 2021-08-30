aadeac98fac4c31568913612f9ac1f7bc564764d
jegomez <go.juangomez23@gmail.com>
2021-08-30 13:48:50 -0500

    ajuste de cron

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 const cron = require('node-cron')
 const nextScheduledPaymentsUseCase = require('./bundle/amortizaciones/application/nextScheduledPaymentsUseCase')
-cron.schedule('* * * * * *', async () => {
-  nextScheduledPaymentsUseCase()
-})
+cron.schedule('0 7 * * *',nextScheduledPaymentsUseCase) // produccion 07:00 am
 
 
