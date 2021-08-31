a553e46c2b7b3a83c207e326e7c59fade306a36a
GilbertoMendoza <profesionalsistemas3@daily-tech.co>
2021-08-31 14:29:08 -0500

    compras correccion mensaje envio correo alerta creacion de solicitudes

--- bundle/compras/application/requestsCreateRankDaysAlertUseCase.js
+++ bundle/compras/application/requestsCreateRankDaysAlertUseCase.js
@@ -9,8 +9,8 @@ module.exports = () => {
       const mail = {
         from: process.env.REQUESTS_EMAIL, // sender address
         to: responseElement.email,
-        text: `Alerta de creación de solicitudes`,
-        subject: `Se encuentra en la base de datos el día de cierre (${dayjs().get('D')}), para la creación de solicitudes, no se encuentra registros de solicitudes`,
+        subject: `Alerta creación de solicitudes`,
+        text: `Hoy es el último día para registrar la solicitud de productos, sino se realiza para este mes no recibirá suministros`,
         documentUrl: process.env.REQUESTS_REGISTER,
         ejs: 'alertaSolicitud'
       }
