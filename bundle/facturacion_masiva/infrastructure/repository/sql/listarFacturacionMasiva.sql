5ef0ab72b7ddc5d17ba0d2bb28984e3c89bf6a1c
ph_jegomez <go.juangomez23@gamil.com>
2022-07-11 09:06:10 -0500

    modificacion de consulta sql

--- bundle/facturacion_masiva/infrastructure/repository/sql/listarFacturacionMasiva.sql
+++ bundle/facturacion_masiva/infrastructure/repository/sql/listarFacturacionMasiva.sql
@@ -1,6 +1,9 @@
-select * 
+select 
+*
 from "FacturacionMasivaDetalles" 
-where (estado = 0 or estado is null) 
-and "serviceLayer" = false
+where (estado = 0 or estado is null)
+and (
+	select count(id) from "FacturacionMasivaDetalles" s1 where "serviceLayer" = true  and (estado = 0 or estado is null)
+) = 0
 order by id
 limit 1
