0c66b5015c2a4d4412b0fddfe4dd0c0acbd077f7
ph_jegomez <go.juangomez23@gamil.com>
2022-08-01 11:42:44 -0500

    facturacion masiva evento

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cron = require('node-cron')
 // const requestsCreateRankDaysAlertUseCase = require('./bundle/compras/application/requestsCreateRankDaysAlertUseCase')
 // const { correosInformativosUseCase } = require('./bundle/sensores/application/correos.informativos.useCase')
 const savefacturacionMasivaSapUseCase = require('./bundle/facturacion_masiva/application/savefacturacionMasivaSapUseCase')
+const saveSapEvento = require('./bundle/facturacion_masiva/application/saveSapEventoUseCase')
 // const saveSegfacturacionSapUseCase = require('./bundle/segupharma_facturacion/application/savefacturacionMasivaSapUseCase')
 
 
@@ -13,6 +14,8 @@ const savefacturacionMasivaSapUseCase = require('./bundle/facturacion_masiva/app
 
 // cron.schedule('* * *', correosInformativosUseCase) // cada hora
 
+console.log('start...')
 cron.schedule('*/1 * * * *', savefacturacionMasivaSapUseCase) // cada 1 minuto
+cron.schedule('*/30 * * * * *', saveSapEvento) // cada 30 segundos
 
 // cron.schedule('*/1 * * * *', saveSegfacturacionSapUseCase) // cada 1 minuto
--- bundle/facturacion_masiva/infrastructure/repository/listarFacturacionMasivaRepository.js
+++ bundle/facturacion_masiva/infrastructure/repository/listarFacturacionMasivaRepository.js
@@ -1,7 +1,7 @@
-const path = require('path')
-const fs = require('fs')
+const path = require('node:path')
+const fs = require('node:fs')
 const { sequelize, Sequelize } = require('../../../../database/models/index')
-module.exports = async () => {
+module.exports = async () => {  
   const sql = fs.readFileSync(path.resolve(__dirname, '.', 'sql', 'listarFacturacionMasiva.sql'), 'utf8')
   const options = {
     type: Sequelize.QueryTypes.SELECT,
