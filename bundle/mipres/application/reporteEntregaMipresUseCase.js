6f05384a47befb925edb230985d9fe90c6ac38ca
Cristian Velasquez <profesionalsistemas1@daily-tech.co>
2023-01-19 07:52:04 -0500

    modificacion: ajuste descarga reporte facturacion

--- bundle/mipres/application/reporteEntregaMipresUseCase.js
+++ bundle/mipres/application/reporteEntregaMipresUseCase.js
@@ -4,14 +4,15 @@ const dayjs = require('dayjs')
 // const { MipresDireccionamiento } = require('../../../database/models')
 const saveReporteMipres = require('../infrastructure/repository/saveReporteMipres.repository')
 
-module.exports = async ({ user}) => {
+module.exports = async () => {
     try {
         const dataWS = await generarWS('SISPRO FACTURACION')
         if (dataWS) {
+            // { fechaInicio: '2023-01-10', fechaFin: '2023-01-10' }}
+            var fecha = dayjs().format('YYYY-MM-DD')
             const { nit, tokken_secundario } = dataWS
             let baseUrl = `https://wsmipres.sispro.gov.co/WSSUMMIPRESNOPBS/api/ReporteEntregaXFecha`
             // reporte entrega direccionamiento en el webservice
-            const fecha = dayjs().format('YYYY-MM-DD')
             await axiosWebService(`${baseUrl}/${nit}/${tokken_secundario}/${fecha}`).then(async (data) => {
                 await saveReporteMipres(data)
             })
--- bundle/mipres/infrastructure/repository/saveReporteMipres.repository.js
+++ bundle/mipres/infrastructure/repository/saveReporteMipres.repository.js
@@ -1,17 +1,42 @@
 const {connect,hanaConfig, execute} = require('../../../../database/hanaClient')
 module.exports = async (body) => {
-    let columns = '("ID","IDReporteEntrega","NoPrescripcion","TipoTec","ConTec","TipoIDPaciente","NoIDPaciente","NoEntrega","CausaNoEntrega","EstadoEntrega","CodTecEntregado","CantTotEntregada","NoLote","FecEntrega","FecRepEntrega","EstRepEntrega","FecAnulacion")'
+    let columns = `("ID","IDReporteEntrega","NoPrescripcion","TipoTec","ConTec","TipoIDPaciente","NoIDPaciente","NoEntrega","EstadoEntrega","CausaNoEntrega", 
+    "ValorEntregado","CodTecEntregado","CantTotEntregada","NoLote","FecEntrega","FecRepEntrega","EstRepEntrega","FecAnulacion")`
     return new Promise(async (resolve, reject) => {
         const conn = await connect()
         conn.connect(hanaConfig, async function (err) {
             if (err) throw err;
-            for (const items of Object.values(body)) {
+            for (const item of Object.values(body)) {
+                // console.log('item--------------------', item)
                 let texto = ''
-                for (const value of Object.values(items)) {
-                    texto += typeof value === 'string' ? `,'${value}'` : `,${value}`;
+                let respuesta = []
+                const sql_id = `SELECT "ID" FROM MP_ENT_MAS WHERE "ID" = '${item.ID}'`
+                const id_mipres = await execute({conn,query:sql_id,params:[]})
+                // console.log('id_mipres--', id_mipres)
+                if (id_mipres[0]) {
+                    let sql_1 = ` UPDATE MP_ENT_MAS SET "IDReporteEntrega" = ${item.IDReporteEntrega}, 
+                    "NoPrescripcion" = '${item.NoPrescripcion}', "TipoTec" = '${item.TipoTec}',  "ConTec" = ${item.ConTec}, 
+                    "TipoIDPaciente" = '${item.TipoIDPaciente}', "NoIDPaciente" = '${item.NoIDPaciente}', "NoEntrega" = ${item.NoEntrega}, 
+                    "EstadoEntrega" = ${item.EstadoEntrega}, "CausaNoEntrega" = ${item.CausaNoEntrega}, "ValorEntregado" = ${item.ValorEntregado}, 
+                    "CodTecEntregado" = '${item.CodTecEntregado}', "CantTotEntregada" = ${item.CantTotEntregada}, 
+                    "NoLote" = ${item.NoLote}, "EstRepEntrega" = ${item.EstRepEntrega} `
+                    if (item.FecRepEntrega) {sql_1 += `, "FecRepEntrega" = '${item.FecRepEntrega}' `}
+                    if (item.FecEntrega) {sql_1 += `, "FecEntrega" = '${item.FecEntrega}' `}
+                    if (item.FecAnulacion) {sql_1 += `, "FecAnulacion" = '${item.FecAnulacion}' `}
+                    sql_1 += ` where "ID" = '${item.ID}'  ` 
+                    respuesta.push('se actusaliza - '+ item.ID)  
+                    // console.log('se actusaliza---', item.ID)
+                    await execute({conn,query:sql_1,params:[]}).catch(err => reject(err)) 
+                    
+                } else {
+                    for (const value of Object.values(item)) {
+                        texto += typeof value === 'string' ? `,'${value}'` : `,${value}`;
+                    }
+                    const sql_1 = `INSERT INTO MP_ENT_MAS ${columns} VALUES (${texto.slice(1)})`
+                    respuesta.push('se crea - '+ item.ID)
+                    // console.log('se guarda nuevo---')
+                    await execute({conn,query:sql_1,params:[]})
                 }
-                const sql_1 = `INSERT INTO MP_DIR ${columns} VALUES (${texto.slice(1)})`
-                await execute({conn,query:sql_1,params:[]})
                 // console.log('sql------', sql_1)
             }
             conn.disconnect()
