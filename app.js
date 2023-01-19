f5a24f7d28504b3148922643304c29d487e2dabf
Cristian Velasquez <profesionalsistemas1@daily-tech.co>
2023-01-19 08:05:07 -0500

    ajuste app mipres

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const savefacturacionMasivaCapita = require('./bundle/facturacion_masiva/applica
 // const saveSapEvento = require('./bundle/facturacion_masiva/application/saveSapEventoUseCase')
 // const saveFacturacionMasivaEventoCapita = require('./bundle/facturacion_masiva/application/saveSapEventoCapita.useCase')
 // const saveSegfacturacionSapUseCase = require('./bundle/segupharma_facturacion/application/savefacturacionMasivaSapUseCase')
+const saveMipres = require('./bundle/mipres/application/listMipresUseCase')
+const reporteEntregaFecha = require('./bundle/mipres/application/reporteEntregaMipresUseCase')
+// const compararDireccionamientoEstado = require('./bundle/mipres/application/compararDireccionamientoMipresUseCase')
 
+// const {hanaTestConnection} = require('./database/hanaClient')
 
 // cron.schedule('0 7 * * *',nextScheduledPaymentsUseCase) // produccion 07:00 am
 // cron.schedule('0 7 * * *', requestsCreateRankDaysAlertUseCase) // produccion 07:00 am
@@ -18,3 +22,9 @@ cron.schedule('*/1 * * * *', savefacturacionMasivaCapita) // cada 1 minuto
 // cron.schedule('* * * * * *', saveSapEvento) // cada 15 segundos
 // cron.schedule('* * * * * *', saveFacturacionMasivaEventoCapita)
 // cron.schedule('*/1 * * * *', saveSegfacturacionSapUseCase) // cada 1 minuto
+cron.schedule('0 7 * * *', saveMipres) // diariamente 07:00 am
+cron.schedule('0 12 * * *', saveMipres) // diariamente 07:00 am
+cron.schedule('0 7 * * *', reporteEntregaFecha) // diariamente 07:00 am
+// cron.schedule('*/1 * * * *', compararDireccionamientoEstado) // cada 1 minuto
+
+// cron.schedule('*/1 * * * *', hanaTestConnection) // cada 1 minuto
