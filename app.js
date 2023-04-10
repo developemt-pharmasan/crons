dca6c9546a003c1102393a3ba26c7ceade81cf10
Cristian Velasquez <profesionalsistemas1@daily-tech.co>
2023-04-10 16:53:23 -0500

    activacion facturacion masiva

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const cron = require('node-cron')
 // const nextScheduledPaymentsUseCase = require('./bundle/amortizaciones/application/nextScheduledPaymentsUseCase')
 // const requestsCreateRankDaysAlertUseCase = require('./bundle/compras/application/requestsCreateRankDaysAlertUseCase')
 // const { correosInformativosUseCase } = require('./bundle/sensores/application/correos.informativos.useCase')
-// const savefacturacionMasivaCapita = require('./bundle/facturacion_masiva/application/savefacturacionMasivaSapUseCase') // CAPITA
+const savefacturacionMasivaCapita = require('./bundle/facturacion_masiva/application/savefacturacionMasivaSapUseCase') // CAPITA
 const saveSapEvento = require('./bundle/facturacion_masiva/application/saveSapEventoUseCase')
-// const saveFacturacionMasivaEventoCapita = require('./bundle/facturacion_masiva/application/saveSapEventoCapita.useCase')
+const saveFacturacionMasivaEventoCapita = require('./bundle/facturacion_masiva/application/saveSapEventoCapita.useCase')
 // const saveSegfacturacionSapUseCase = require('./bundle/segupharma_facturacion/application/savefacturacionMasivaSapUseCase')
 // const saveMipres = require('./bundle/mipres/application/listMipresUseCase')
 // const reporteEntregaFecha = require('./bundle/mipres/application/reporteEntregaMipresUseCase')
@@ -19,11 +19,11 @@ const saveSapEvento = require('./bundle/facturacion_masiva/application/saveSapEv
 // cron.schedule('* * *', correosInformativosUseCase) // cada hora
 
 console.log('start...')
-// cron.schedule('*/2 * * * 1-5', savefacturacionMasivaCapita) // cada 1 minuto
-// cron.schedule('*/50 * * * * 6,0', savefacturacionMasivaCapita) // cada 50 segundos sabado y domingo
-// cron.schedule('* * * * * *', saveSapEvento) // cada 15 segundos
-// cron.schedule('* * * * * *', saveFacturacionMasivaEventoCapita)
-cron.schedule('*/1 * * * *', saveSapEvento) // cada 20 minutos
+cron.schedule('*/2 * * * 1-5', savefacturacionMasivaCapita) // cada 1 minuto
+cron.schedule('*/50 * * * * 6,0', savefacturacionMasivaCapita) // cada 50 segundos sabado y domingo
+cron.schedule('* * * * * *', saveSapEvento) // cada segundo
+cron.schedule('* * * * * *', saveFacturacionMasivaEventoCapita) // cada segundo
+// cron.schedule('*/1 * * * *', saveSapEvento) // cada 20 minutos
 
 
 // cron.schedule('*/1 * * * *', saveSegfacturacionSapUseCase) // cada 1 minuto
